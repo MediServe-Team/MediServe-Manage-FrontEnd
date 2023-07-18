dad984fbc3c026672589d0ab3b9297a17f894d1c
Hoàng Văn Phúc <20521760@gm.uit.edu.vn>
2023-07-18 23:36:37 +0700

    handle update delete account customer

--- src/services/accountServices.js
+++ src/services/accountServices.js
@@ -9,3 +9,25 @@ export const getAllAccountService = async (axiosWithToken) => {
   const response = await axiosWithToken.get('/users/all');
   return response.data;
 };
+
+export const getAccountByIdService = async (axiosWithToken, userId) => {
+  const response = await axiosWithToken.get(`/users/${userId}`);
+  return response.data;
+};
+
+export const updateAccountByIdService = async (axiosWithToken, userId, data) => {
+  const response = await axiosWithToken.put(`/users/update-user-info/${userId}`, {
+    name: data?.name,
+    fullName: data?.fullName,
+    address: data?.address,
+    age: data?.age,
+    dateOfBirth: data?.dateOfBirth,
+    phoneNumber: data?.phoneNumber,
+  });
+  return response.data;
+};
+
+export const deleteAccountService = async (axiosWithToken, userId) => {
+  const response = await axiosWithToken.delete(`/users/${userId}`);
+  return response.data;
+};
